c38b210f1de4946b136ad6c828178e37cf995f24
shivam <shivamsharmasvm20@gmail.com>
2020-11-29 16:08:50 +0530

    Initial code setup

---
+++ mycart-core/src/main/java/com/mycart/core/MyCartCoreApplication.java
@@ -0,0 +1,12 @@
+package com.mycart.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCartCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCartCoreApplication.class, args);
+    }
+}
---
+++ mycart-core/src/test/java/com/mycart/core/MyCartCoreApplicationTest.java
@@ -0,0 +1,28 @@
+package com.mycart.core;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MyCartCoreApplication.class)
+@WebAppConfiguration
+public class MyCartCoreApplicationTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    public void testDummy() throws SQLException{
+        String schema = dataSource.getConnection().getCatalog();
+        assertEquals("mycart", schema);
+    }
+}
